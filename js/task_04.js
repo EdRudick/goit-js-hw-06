a1f60616106ba1a197ce4ff1ffc93e5e4aa66626
EdRudick <erudnytdky@gmail.com>
2023-09-07 21:21:26 +0300

    0709

--- js/task_04.js
+++ js/task_04.js
@@ -18,12 +18,3 @@ const decrement = () => {
 };
 wrap_button.sub.addEventListener("click", increment);
 wrap_button.add.addEventListener("click", decrement);
-
-const nameInput = document.querySelector("#name-input");
-const nameOutput = document.querySelector("#name-output");
-
-nameInput.addEventListener("input", onInputChange);
-
-function onInputChange({ target: { value } }) {
-  nameOutput.textContent = value || "Anonymous";
-}
--- js/task_05.js
+++ js/task_05.js
@@ -1,9 +1,16 @@
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
+
+nameInput.addEventListener("input", onInputChange);
+
+function onInputChange({ target: { value } }) {
+  nameOutput.textContent = value || "Anonymous";
+}
+
 const inputName = document.getElementById("name-input");
 const outputName = document.getElementById("name-output");
-
 const placeholder = (event) => {
   outputName.textContent =
     inputName.value !== "" ? event.currentTarget.value : "Anonymous";
 };
-
 inputName.addEventListener("input", placeholder);
